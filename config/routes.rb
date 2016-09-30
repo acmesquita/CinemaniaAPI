4f4fbe063ada5a850934e23505d83a63a8dd1035
acmesquita <catha.ana.1994@gmail.com>
2016-09-30 07:32:06 -0300

    ajuste das rotas na configuracao

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :filmes
-  resources :sessaos
-  resources :salas
-  resources :cinemas
+  resources :cinemas do
+    resources :salas do
+      resources :sessaos do
+        resources :filmes
+      end
+    end
+  end
 
   root 'cinemas#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
